b41c01fb878c4666e81db3346a30ce987f31a8a6
ivanovskigligor <ivanovskigligor@gmail.com>
2023-05-17 02:00:01 +0200

    initial

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes, Link}  from 'react-router-dom'
+import Home from './pages/Home'
+import CreatePost from "./pages/CreatePost"
+import Post from "./pages/Post"
+
+function App() {
+  return (
+  <div className='App'>
+    <Router>
+      <div className='navbar'>
+      <Link to="/createpost">Create Post</Link>
+      <Link to="/">Home</Link>
+      </div>
+      <Routes>
+        <Route path = "/" exact element = {<Home/>}/>
+        <Route path = "/createpost" exact element = {<CreatePost/>}/>
+        <Route path = "/post/:id" exact element = {<Post/>}/>
+      </Routes>
+    </Router>
+  </div>
+  );
+}
+
+export default App;
